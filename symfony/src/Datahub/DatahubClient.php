ebed35282eab5a406b5611422e4eceaa4a9f98ec
i.lapushkov <i.lapushkov@articul.ru>
2023-03-19 21:15:49 +0400

    declare(strict_types=1)

--- symfony/src/Datahub/DatahubClient.php
+++ symfony/src/Datahub/DatahubClient.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Datahub;
 
 use App\Nasdaq\Client;
--- symfony/src/Nasdaq/CompanyModel.php
+++ symfony/src/Nasdaq/CompanyModel.php
@@ -1,9 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Nasdaq;
 
 interface CompanyModel
 {
     public function getCompanyName(): string;
+
     public function getSymbol(): string;
 }
