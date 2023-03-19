2a39d98c74db15250d063e7dee5fd5ce92e2bb03
i.lapushkov <i.lapushkov@articul.ru>
2023-03-19 18:06:44 +0400

    validate form's fields

--- symfony/src/Controller/CompanyInformationController.php
+++ symfony/src/Controller/CompanyInformationController.php
@@ -4,15 +4,17 @@ namespace App\Controller;
 
 use App\Form\CompanyInformationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CompanyInformationController extends AbstractController
 {
     #[Route('', name: 'app_company_information')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $form = $this->createForm(CompanyInformationFormType::class);
+        $form->handleRequest($request);
 
         return $this->render(
             'index.html.twig',
--- symfony/src/Form/CompanyInformationFormType.php
+++ symfony/src/Form/CompanyInformationFormType.php
@@ -9,13 +9,22 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class CompanyInformationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('company_symbol', TextType::class, ['label' => 'Company Symbol'])
+            ->add(
+                'company_symbol',
+                TextType::class,
+                [
+                    'label' => 'Company Symbol',
+                    'constraints' => new NotBlank(message: 'Company Symbol is required')
+                ]
+            )
             ->add(
                 'start_date',
                 DateType::class,
@@ -23,7 +32,8 @@ class CompanyInformationFormType extends AbstractType
                     'widget' => 'single_text',
                     'html5' => false,
                     'attr' => ['class' => 'datepicker'],
-                    'label' => 'Start Date'
+                    'label' => 'Start Date',
+                    'constraints' => new NotBlank(message: 'Start Date is required')
                 ]
             )
             ->add(
@@ -33,10 +43,20 @@ class CompanyInformationFormType extends AbstractType
                     'widget' => 'single_text',
                     'html5' => false,
                     'attr' => ['class' => 'datepicker'],
-                    'label' => 'End Date'
+                    'label' => 'End Date',
+                    'constraints' => new NotBlank(message: 'End Date is required')
                 ]
             )
-            ->add('email', EmailType::class)
+            ->add(
+                'email',
+                EmailType::class,
+                [
+                    'constraints' => [
+                        new NotBlank(message: 'Email is required'),
+                        new Email(message: 'Email is not valid')
+                    ]
+                ]
+            )
             ->add('submit', SubmitType::class, ['label' => 'Submit Form']);
     }
 
