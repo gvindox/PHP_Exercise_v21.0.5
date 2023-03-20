495bb22f4500c120ba736a74648e84fdb1303e17
i.lapushkov <i.lapushkov@articul.ru>
2023-03-20 13:32:26 +0400

    app:load-company-information command in startup.sh

--- startup.sh
+++ startup.sh
@@ -5,4 +5,5 @@ cp symfony/.env.example symfony/.env
 cp symfony/.env.test.example symfony/.env.test
 docker-compose exec php composer install
 docker-compose exec php bin/console doctrine:migration:migrate --no-interaction
+docker-compose exec php bin/console app:load-company-information
 docker-compose exec php bin/console cache:clear
