4a5a0754d0c2f78f6e45e2eff2b1428815900e58
i.lapushkov <i.lapushkov@articul.ru>
2023-03-20 12:14:51 +0400

    historical data page

--- symfony/src/Controller/CompanyInformationController.php
+++ symfony/src/Controller/CompanyInformationController.php
@@ -5,24 +5,64 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Form\CompanyInformationFormType;
+use App\Form\CompanyInformationModel;
+use App\HistoricalData\HistoricalDataServiceInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CompanyInformationController extends AbstractController
 {
-    #[Route('', name: 'app_company_information')]
+    public function __construct(private HistoricalDataServiceInterface $historicalDataService)
+    {
+    }
+
+    #[Route('', name: 'index_page_form')]
     public function index(Request $request): Response
     {
         $form = $this->createForm(CompanyInformationFormType::class);
         $form->handleRequest($request);
 
+        return $this->renderPage($form);
+    }
+
+    private function renderPage(FormInterface $form): Response
+    {
+        $renderPage = $this->renderIndexPage($form);
+
+        if ($this->isNeedToRenderHistoricalDataPage($form)) {
+            $renderPage = $this->renderHistoricalDataPage($form);
+        }
+
+        return $renderPage;
+    }
+
+    private function renderIndexPage(FormInterface $form): Response
+    {
         return $this->render(
             'index.html.twig',
             [
                 'form' => $form
             ]
         );
+    }
+
+    private function renderHistoricalDataPage(FormInterface $form): Response
+    {
+        $historicalData = $this->historicalDataService->getHistoricalData($form->getData());
+
+        return $this->render(
+            'historical-data.html.twig',
+            [
+                'historical_data' => $historicalData
+            ]
+        );
+    }
+
+    private function isNeedToRenderHistoricalDataPage(FormInterface $form): bool
+    {
+        return $form->isSubmitted() && $form->isValid() && $form->getData() instanceof CompanyInformationModel;
     }
 }
