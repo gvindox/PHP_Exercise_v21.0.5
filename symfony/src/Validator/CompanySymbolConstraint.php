bd3b565b29532246528d0ced78a018fdebc787d1
i.lapushkov <i.lapushkov@articul.ru>
2023-03-20 13:37:16 +0400

    validatedBy(): string

--- symfony/src/Validator/CompanySymbolConstraint.php
+++ symfony/src/Validator/CompanySymbolConstraint.php
@@ -12,7 +12,7 @@ class CompanySymbolConstraint extends Constraint
     public const STRING_PARAM = '{{ string }}';
     public string $message = 'The company symbol "'.self::STRING_PARAM.'" is not valid.';
 
-    public function validatedBy()
+    public function validatedBy(): string
     {
         return CompanySymbolValidator::class;
     }
